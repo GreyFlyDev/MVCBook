bf495752387c60a15f9336b61181d9f385c525b5
Greg Flynn <gflynn@covdnssrv.co.volusia.fl.us>
2016-05-10 10:46:45 -0400

    Added AutoProperty Action Method to HomeController along with its View

--- Chapter4/Chapter4/Controllers/HomeController.cs
+++ Chapter4/Chapter4/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+//Add refernce to object
+using Chapter4.Models;
 
 namespace Chapter4.Controllers
 {
@@ -11,7 +13,23 @@ namespace Chapter4.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            return View("Result");
+        }
+
+        public ActionResult AutoProperty()
+        {
+            //create new product object
+            Product myProduct = new Product();
+
+            //Set property value 
+            myProduct.Name = "Kayak";
+
+            //get property
+            string productName = myProduct.Name;
+
+            //Generate View
+            return View("Result", (Object)String.Format("Product Name: {0}", productName));
+
         }
     }
 }
